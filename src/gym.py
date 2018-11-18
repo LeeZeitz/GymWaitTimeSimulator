bfe5b9e8cd9c490e03a6bbb8104184a965b74a61
LeeZeitz <andrew.l.zeitz@gmail.com>
2018-11-17 19:19:31 -0800

    tweak model parameters, collect wait time statistics

--- src/gym.py
+++ src/gym.py
@@ -1,24 +1,26 @@
 import simpy
 import random
+import numpy as np
 
 NUMBER_OF_FREE_WEIGHTS = 5
-NUMBER_OF_RACKS = 3
-NUMBER_OF_MACHINES = 4
+NUMBER_OF_RACKS = 2
+NUMBER_OF_MACHINES = 3
 NUMBER_OF_BIKES = 50
 NUMBER_OF_CARDIO = 20
 NUMBER_OF_BENCHES = 2
 
 FREE_WEIGHT_TIME = 30
-BIKE_TIME = 30
-CARDIO_TIME = 60
-MACHINE_TIME = 30
-RACK_TIME = 15
-BENCH_TIME = 15
+BIKE_TIME = 35.6
+CARDIO_TIME = 52.2
+MACHINE_TIME = 28.05
+RACK_TIME = 12.61
+BENCH_TIME = 8.57
 
 class Gym(object):
 
-    def __init__(self, env):
+    def __init__(self, env, stream):
         self.env = env
+        self.stream = stream
         self.weights = simpy.Resource(env, capacity=NUMBER_OF_FREE_WEIGHTS)
         self.racks = simpy.Resource(env, capacity=NUMBER_OF_RACKS)
         self.machines = simpy.Resource(env, capacity=NUMBER_OF_MACHINES)
@@ -28,25 +30,19 @@ class Gym(object):
 
     
     def lift_free_weights(self, id):
-        yield self.env.timeout(FREE_WEIGHT_TIME)
-        print ('Customer {0} leaving free weights at {1:2f}...'.format(id, self.env.now))
+        yield self.env.timeout(self.stream.exponential(FREE_WEIGHT_TIME))
 
     def use_rack(self, id):
-        yield self.env.timeout(RACK_TIME)
-        print ('Customer {0} leaving a rack at {1:2f}...'.format(id, self.env.now))
+        yield self.env.timeout(self.stream.exponential(RACK_TIME))
 
     def do_cardio(self, id):
-        yield self.env.timeout(CARDIO_TIME)
-        print ('Customer {0} leaving cardio at {1:2f}'.format(id, self.env.now))
+        yield self.env.timeout(self.stream.exponential(CARDIO_TIME))
 
     def ride_bike(self, id):
-        yield self.env.timeout(BIKE_TIME)
-        print ('Customer {0} leaving bike at {1:2f}'.format(id, self.env.now))
+        yield self.env.timeout(self.stream.exponential(BIKE_TIME))
 
     def weight_machines(self, id):
-        yield self.env.timeout(MACHINE_TIME)
-        print ('Customer {0} leaving machines at {1:2f}'.format(id, self.env.now))
+        yield self.env.timeout(self.stream.exponential(MACHINE_TIME))
 
     def bench_press(self, id):
-        yield self.env.timeout(BENCH_TIME)
-        print ('Customer {0} leaving bench at {1:2f}'.format(id, self.env.now))
+        yield self.env.timeout(self.stream.exponential(BENCH_TIME))
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from gym import Gym
 import random
 import numpy as np
 import copy
+import sys
 
 # 50% men and 50% women
 # women: 50% use only cardio, 10% use only weights, 40% use both
@@ -10,95 +11,154 @@ import copy
 # men: 10% use only cardio, 80% use only weights, 10% use both
             # 60% use racks
 
-SIM_TIME = 1000
-INTERARRAVAL_TIME = 10
+SIM_TIME = 10000
+INTERARRAVAL_TIME = 5
+NUMBER_OF_SIMULATIONS = 5
+NUMBER_OF_CUSTOMERS = [0 for i in range(NUMBER_OF_SIMULATIONS)]
 
-def weights(id, env, gym):
+wait_times = [{
+    'cardio_wait_time': 0,
+    'bench_wait_time': 0,
+    'machines_wait_time': 0,
+    'rack_wait_time': 0,
+    'free_weights_wait_time': 0
+} for i in range(NUMBER_OF_SIMULATIONS)]
+
+def weights(id, env, gym, wait_times):
+    arrive_time = env.now
     with gym.weights.request() as request:
         yield request
-        print ('Customer {0} starts using free weights at {1:2f}'.format(id, env.now))
+        #print ('Customer {0} starts using free weights at {1:2f}'.format(id, env.now))
+        start_time = env.now
+        wait_times['free_weights_wait_time'] += start_time - arrive_time
         yield env.process(gym.lift_free_weights(id))
-        print ('Customer {0} finished using free weights at {1:2f}'.format(id, env.now))
+        #print ('Customer {0} finished using free weights at {1:2f}'.format(id, env.now))
 
-def cardio(id, env, gym):
+def cardio(id, env, gym, wait_times):
+    arrive_time = env.now
     with gym.cardio.request() as request:
         yield request
-        print ('Customer {0} starts using cardio at {1:2f}'.format(id, env.now))
+        #print ('Customer {0} starts using cardio at {1:2f}'.format(id, env.now))
+        start_time = env.now
+        wait_times['cardio_wait_time'] += start_time - arrive_time
         yield env.process(gym.do_cardio(id))
-        print ('Customer {0} finished doing cardio at {1:2f}'.format(id, env.now))
+        #print ('Customer {0} finished doing cardio at {1:2f}'.format(id, env.now))
 
-def racks(id, env, gym):
+def racks(id, env, gym, wait_times):
+    arrive_time = env.now
     with gym.racks.request() as request:
         yield request
-        print ('Customer {0} starts using rack at {1:2f}'.format(id, env.now))
+        #print ('Customer {0} starts using rack at {1:2f}'.format(id, env.now))
+        start_time = env.now
+        wait_times['rack_wait_time'] += start_time - arrive_time
         yield env.process(gym.use_rack(id))
-        print ('Customer {0} finished using rack at {1:2f}'.format(id, env.now))
+        #print ('Customer {0} finished using rack at {1:2f}'.format(id, env.now))
 
-def machines(id, env, gym):
+def machines(id, env, gym, wait_times):
+    arrive_time = env.now
     with gym.machines.request() as request:
         yield request
-        print ('Customer {0} starts using weight machines at {1:2f}'.format(id, env.now))
+        #print ('Customer {0} starts using weight machines at {1:2f}'.format(id, env.now))
+        start_time = env.now
+        wait_times['machines_wait_time'] += start_time - arrive_time
         yield env.process(gym.weight_machines(id))
-        print ('Customer {0} finished using weight machines at {1:2f}'.format(id, env.now))
+        #print ('Customer {0} finished using weight machines at {1:2f}'.format(id, env.now))
 
-def benches(id, env, gym):
-    with gym.machines.request() as request:
+def benches(id, env, gym, wait_times):
+    arrive_time = env.now
+    with gym.benches.request() as request:
         yield request
-        print ('Customer {0} starts using a bench at {1:2f}'.format(id, env.now))
-        yield env.process(gym.benches(id))
-        print ('Customer {0} finished using a bench at {1:2f}'.format(id, env.now))
+        #print ('Customer {0} starts using a bench at {1:2f}'.format(id, env.now))
+        start_time = env.now
+        wait_times['bench_wait_time'] += start_time - arrive_time
+        yield env.process(gym.bench_press(id))
+        #print ('Customer {0} finished using a bench at {1:2f}'.format(id, env.now))
 
 weight_activities = [weights, racks, machines, benches]
 
 
 
-def athlete(env, id, gym):
-    print('Customer {0} has arrived at the gym at {1:2f}'.format(id, env.now))
+def athlete(env, id, gym, wait_times, stream):
+    #print('Customer {0} has arrived at the gym at {1:2f}'.format(id, env.now))
 
-    r = random.randint(0, 100)
+    r = stream.randint(0, 100)
 
-    # Athlete does cardio
-    if r < 55:
-        yield cardio(id, env, gym)
+    # Athlete does cardio (56% of them do)
+    if r < 58:
 
-        # Lift weights as well as doing cardio
-        if r < 25:
+        yield env.process(cardio(id, env, gym, wait_times))
+
+        # Lift weights as well as doing cardio (32% do)
+        if r < 32:
             activities = copy.deepcopy(weight_activities)
             # Do two weight lifting activities
-            i = random.randint(0, 3)
+            i = stream.randint(0, 3)
             j = i
             while j == i:
-                j = random.randint(0, 3)
+                j = stream.randint(0, 3)
 
-            yield activities[i](id, env, gym)
-            yield activities[j](id, env, gym)
-
-    # Athlete only lifts weights
+            yield env.process(activities[i](id, env, gym, wait_times))
+            yield env.process(activities[j](id, env, gym, wait_times))
+   
+    # Athlete only lifts weights, no cardio (42% of them do)
     else:
         activities = copy.deepcopy(weight_activities)
         # Do three weight lifting activities
-        i = random.randint(0, 3)
+        i = stream.randint(0, 3)
         del activities[i]
 
         for activity in activities:
-            yield activity(id, env, gym)
+            yield env.process(activity(id, env, gym, wait_times))
 
 
-def setup(env):
-    gym = Gym(env)
-    for i in range(4):
-        env.process(athlete(env, i, gym))
+def setup(env, stream, NUMBER_OF_CUSTOMERS, n):
+    gym = Gym(env, stream)
+    for i in range(2):
+        env.process(athlete(env, i, gym, wait_times[n], stream))
+        NUMBER_OF_CUSTOMERS[n] += 1
 
     while True:
-        yield env.timeout(random.randint(INTERARRAVAL_TIME - 2, INTERARRAVAL_TIME + 2))
+        yield env.timeout(stream.exponential(INTERARRAVAL_TIME))
         i += 1
-        env.process(athlete(env, i, gym))
+        NUMBER_OF_CUSTOMERS[n] += 1
+        env.process(athlete(env, i, gym, wait_times[n], stream))
+
+
+def results(num_cust, n):
+    print ()
+    print ('Number of Customers: {0}'.format(NUMBER_OF_CUSTOMERS[0]))
+    print ('Simulation Length:   {0}'.format(SIM_TIME))
+    print ('Average Wait Times: ')
+    print ('    Cardio:         {0}'.format(wait_times[n]['cardio_wait_time']/num_cust))
+    print ('    Machines:       {0}'.format(wait_times[n]['machines_wait_time']/num_cust))
+    print ('    Free Weights:   {0}'.format(wait_times[n]['free_weights_wait_time']/num_cust))
+    print ('    Bench:          {0}'.format(wait_times[n]['bench_wait_time']/num_cust))
+    print ('    Rack:           {0}'.format(wait_times[n]['rack_wait_time']/num_cust))
+    print ()
 
 
 if __name__ == '__main__':
 
-    env = simpy.Environment()
-    env.process(setup(env))
+    try:
+        seed = sys.argv[1]
+    except:
+        seed = input('Please provide a seed: \n')
 
-    env.run(until=SIM_TIME)
+    if len(sys.argv) < 6:
+        print ('need more seeds please')
+        exit()
+
+    seeds = sys.argv[1:6]
+
+    print (wait_times)
+
+
+    for n in range (NUMBER_OF_SIMULATIONS):
+        stream = np.random.RandomState(int(seeds[n]))
+        env = simpy.Environment()
+        env.process(setup(env, stream, NUMBER_OF_CUSTOMERS, n))
+        env.run(until=SIM_TIME)
+        results(NUMBER_OF_CUSTOMERS[n], n)
+
+
 
